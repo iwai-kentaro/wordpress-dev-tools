72ed1723085ab5dfd35965e3b44d93cc96b40108
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-04 21:54:25 +0900

    やっぱり必要なやつ

--- browsersync.js
+++ browsersync.js
@@ -1,7 +1,10 @@
 const browserSync = require('browser-sync').create();
+require('dotenv').config(); // .env ファイルを読み込む
+
+const PORT = process.env.WORDPRESS_PORT || 8000; // 環境変数から取得（デフォルト: 8000）
 
 browserSync.init({
-    proxy: "http://localhost:8000/wp-admin", // WordPressサイトのURL
+    proxy: `http://localhost:${PORT}`, // WordPressサイトのURL
     open: true, // ブラウザを自動で開く
     files: [
         "wordpress/wp-content/themes/my-themes/**/*.php",
